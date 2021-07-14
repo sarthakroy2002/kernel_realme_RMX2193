b4dd5c4009089392d03f6917d305cb5fab6350c5
sarthakroy2002 <sarthak.roy@s.amity.edu>
2021-07-14 15:54:55 +0000

    Import drivers/scsi/ from RealmeNarzo20-Q-OSS
    Change-Id: Id3477e090dd54dae1d35ab52a63de7d51103ad5e

--- drivers/scsi/ufs/ufs-mtk.c
+++ drivers/scsi/ufs/ufs-mtk.c
@@ -1042,10 +1042,6 @@ static int ufs_mtk_init_mphy(struct ufs_hba *hba)
 
 static int ufs_mtk_enable_crypto(struct ufs_hba *hba)
 {
-	/* avoid resetting host during resume flow or when link is not off */
-	if (hba->pm_op_in_progress || !ufshcd_is_link_off(hba))
-		return 0;
-
 	/* restore vendor crypto setting by re-using resume operation */
 	mt_secure_call(MTK_SIP_KERNEL_HW_FDE_UFS_CTL, (1 << 2), 0, 0, 0);
 
@@ -1103,6 +1099,24 @@ int ufs_mtk_linkup_fail_handler(struct ufs_hba *hba, int left_retry)
 	return 0;
 }
 
+int ufs_mtk_check_powerctl(struct ufs_hba *hba)
+{
+	int err = 0;
+	u32 val = 0;
+
+	/* check if host in power saving */
+	err = ufshcd_dme_get(hba,
+		UIC_ARG_MIB(VENDOR_UNIPROPOWERDOWNCONTROL), &val);
+	if (!err && val == 0x1) {
+		err = ufshcd_dme_set(hba,
+			UIC_ARG_MIB(VENDOR_UNIPROPOWERDOWNCONTROL), 0);
+		dev_info(hba->dev, "get dme 0x%x = %d, set 0 (%d)\n",
+			VENDOR_UNIPROPOWERDOWNCONTROL, val, err);
+	}
+
+	return err;
+}
+
 static int ufs_mtk_hce_enable_notify(struct ufs_hba *hba,
 	enum ufs_notify_change_status stage)
 {
@@ -1144,6 +1158,11 @@ static int ufs_mtk_pre_link(struct ufs_hba *hba)
 
 	/* ensure auto-hibern8 is disabled during hba probing */
 	ufshcd_vops_auto_hibern8(hba, false);
+
+	/* powerup unipro if unipro powerdown */
+	ret = ufs_mtk_check_powerctl(hba);
+	if (ret)
+		return ret;
 
 	/* configure deep stall */
 	ret = ufshcd_dme_get(hba, UIC_ARG_MIB(VENDOR_SAVEPOWERCONTROL), &tmp);
@@ -1219,8 +1238,21 @@ static int ufs_mtk_suspend(struct ufs_hba *hba, enum ufs_pm_op pm_op)
 		 */
 		ret = ufshcd_dme_set(hba,
 			UIC_ARG_MIB_SEL(VENDOR_UNIPROPOWERDOWNCONTROL, 0), 1);
-		if (ret)
+		if (ret) {
+			/* dump ufs debug Info like XO_UFS/VEMC/VUFS18 */
+			ufs_mtk_pltfrm_gpio_trigger_and_debugInfo_dump(hba);
+
+			/*
+			 * Power down fail leave vendor-specific power down mode
+			 * to resume UniPro state
+			 */
+			(void)ufshcd_dme_set(hba,
+				UIC_ARG_MIB_SEL(VENDOR_UNIPROPOWERDOWNCONTROL,
+				0), 0);
+			ret = -EAGAIN;
+
 			return ret;
+		}
 
 		ufs_mtk_pltfrm_suspend(hba);
 
@@ -2112,6 +2144,33 @@ int ufs_mtk_auto_hiber8_quirk_handler(struct ufs_hba *hba, bool enable)
 
 	return 0;
 }
+
+int ufs_mtk_wait_link_state(struct ufs_hba *hba, u32 *state,
+			    unsigned long retry_ms)
+{
+	unsigned long timeout;
+	u32 val;
+
+	timeout = jiffies + msecs_to_jiffies(retry_ms);
+	do {
+		ufshcd_writel(hba, 0x20, REG_UFS_MTK_DEBUG_SEL);
+		val = ufshcd_readl(hba, REG_UFS_MTK_PROBE);
+		val = val >> 28;
+
+		if (val == *state)
+			break;
+
+		/* sleep for max. 200us */
+		usleep_range(100, 200);
+	} while (time_before(jiffies, timeout));
+
+	if (val == *state)
+		return 0;
+
+	*state = val;
+	return -ETIMEDOUT;
+}
+
 
 /* Notice: this function must be called in automic context */
 /* Because it is not protected by ufs spin_lock or mutex */
